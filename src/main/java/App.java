7f6a9458eb400335e43eab4bb821676c98481174
Svyatoslav Prohorenkov <svyatprohorenkov@gmail.com>
2024-11-05 17:29:59 +0300

    Реализация

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,12 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("HelloWorld beans are the same: " + (helloWorld1 == helloWorld2));
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Cat beans are the same: " + (cat1 == cat2));
     }
 }
